f6d64ee79b3d936543fb33651882d306bec1dc7d
Rollando Mkolwe <109596260+odnallor@users.noreply.github.com>
2024-03-05 16:41:18 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,6 +1,8 @@
 #user name input
 name  = input("Enter Your Name: ")
+#user age input
 age = input("Enter Your Age:")
+#user location input
 location = input("Enter Your Location:")
 
 print("Hello",name,"you are",age, "years old and live in",location)
