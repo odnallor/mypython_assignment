f8cd7463c28783a2391ba164fb4cff2c1093326c
Rollando Mkolwe <109596260+odnallor@users.noreply.github.com>
2024-03-05 16:40:24 +0300

    Add files via upload
    assignment 1

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#user name input
+name  = input("Enter Your Name: ")
+age = input("Enter Your Age:")
+location = input("Enter Your Location:")
+
+print("Hello",name,"you are",age, "years old and live in",location)
